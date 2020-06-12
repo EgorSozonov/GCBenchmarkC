142fc4e0bf37979c62ff93ea8d68a90be10db82a
a <Egor.Sozonov.09@gmail.com>
2020-06-12 13:58:40 +0300

    0
    0

--- src/GCBenchmark.c
+++ src/GCBenchmark.c
@@ -5,6 +5,7 @@
 #define push(sp, n) (*((sp)++) = (n))
 #define pop(sp) (*--(sp))
 #define peek(sp) (*(sp))
+#define stackSize(sp, stack) ((sp)-(stack))
 
 const int HEIGHT = 2;
 const int SIZE_REGION = 200000;
@@ -16,58 +17,26 @@ struct Tree {
 };
 
 
+struct Tree* createLeftTree(int payload[4], struct Tree** stck) {
+    return NULL;
+}
 
-void createTree(int payload[4]) {
+struct Tree* createTree(int payload[4]) {
     // The stack
-    struct Tree* stack[] = malloc(HEIGHT * sizeof(struct Tree*));
+    struct Tree** stack = malloc(HEIGHT * sizeof(struct Tree*));
     struct Tree** sp = stack;
-
     struct Tree* wholeTree = createLeftTree(payload, sp);
-}
-
-Tree* createLeftTree(payload, stck) {
-    return null;
-}
-
-fun createTree(payload: Array<Int>) {
-    if (height <= 0) return;
-    val stack = Stack<Loc>()
-    val wholeTree = createLeftTree(height, payload, stack)
-    while (stack.any()) {
-        var bottomElement = stack.peek()
-            if (bottomElement.arr[bottomElement.ind + 1] > -1 || stack.count() == height) {
-                stack.pop()
-                    while (stack.any()) {
-                        bottomElement = stack.peek()
-                            if (bottomElement.arr[bottomElement.ind + 1] == -1) break
-                                stack.pop()
-                    }
-            }
-        if (stack.any()) {
-            bottomElement = stack.peek()
-                bottomElement.arr[bottomElement.ind + 1] = createLeftTree(height - stack.count(), payload, stack)
-        }
+    printf("stack size is %d", stackSize(sp, stack));
+    while (sp > stack) {
+        //struct Tree* bottomElement = peek(sp);
+        //if (stackSize(sp, stack) == HEIGHT)
     }
+    return wholeTree;
+    
 }
 
 
-class Loc(val arr : Array<Int>, val ind : Int) {}
 
-
-fun createLeftTree(height : Int, payload : Array<Int>, stack : Stack<Loc>) : Int {
-    if (height == 0) return -1
-
-        val wholeTree = allocateNode(payload)
-        var currTree : Loc = toLoc(wholeTree)
-        stack.push(currTree)
-        for (i : Int in 1 until height) {
-            val newTree = allocateNode(payload)
-                currTree.arr[currTree.ind] = newTree
-                currTree = toLoc(newTree)
-                stack.push(currTree)
-        }
-    return wholeTree
-}
 
 
 int main(int argc, char** argv) {
