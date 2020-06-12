00f4c47b7d7c372e165fbcd491d832a2481dbca8
a <Egor.Sozonov.09@gmail.com>
2020-06-12 12:34:32 +0300

    0

--- src/GCBenchmark.c
+++ src/GCBenchmark.c
@@ -6,18 +6,18 @@ const HEIGHT = 2;
 #define SIZE_REGION 200000;
 
 struct Tree {
-	struct Tree* left;
-	struct Tree* right;
-	int vals[4];
+    struct Tree* left;
+    struct Tree* right;
+		int vals[4];
 };
 
 
 int main(int argc, char** argv) {
-	int numRegions = HEIGHT/SIZE_REGION + 1;
-	printf("numRegions = %d", numRegions);
-	struct Tree** regions = (struct Tree**)malloc(numRegions * sizeof(Tree*));
+    int numRegions = HEIGHT/SIZE_REGION + 1;
+		printf("numRegions = %d", numRegions);
+		struct Tree** regions = (struct Tree**)malloc(numRegions * sizeof(Tree*));
 
 
-	return 0;
+		return 0;
 }
 
